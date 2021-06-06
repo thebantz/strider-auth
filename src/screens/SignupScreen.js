329be4e3da7e5c773879b0877f64d7c04aff016f
“thebantz” <nicoledbanta@gmail.com>
2021-06-06 15:28:06 -0700

    create reusable AuthForm component

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,43 +1,29 @@
-import React, { useState, useContext } from "react";
+import React, {  useContext } from "react";
 import { View, StyleSheet, TouchableOpacity } from "react-native";
-import { Button, Text, Input } from "react-native-elements";
+import { Text } from "react-native-elements";
 import Spacer from "../components/Spacer";
 
 import { Context as AuthContext } from "../context/AuthContext";
+import AuthForm from "../components/AuthForm";
 
 const SignupScreen = ({ navigation }) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const { state, signup } = useContext(AuthContext);
 
   return (
     <View style={styles.container}>
-      <Spacer>
-        <Text h3>Sign up for Tracker</Text>
-      </Spacer>
-      <Input
-        label="Email"
-        value={email}
-        onChangeText={setEmail}
-        autoCapitalize="none"
-        autoCorrect={false}
+      <AuthForm
+        headerText="Sign Up for Strider"
+        errorMessage={state.errorMessage}
+        submitButtonText="Sign Up"
+        onSubmit={signup}
       />
-      <Spacer />
-      <Input
-        label="Password"
-        value={password}
-        onChangeText={setPassword}
-        autoCapitalize="none"
-        autoCorrect={false}
-        secureTextEntry
-      />
-      {state.errorMessage ? (
-        <Text style={styles.errorMessage}>{state.errorMessage}</Text>
-      ) : null}
-      <Spacer>
-        <Button title="Sign Up" onPress={() => signup({ email, password })} />
-      </Spacer>
-      <TouchableOpacity></TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate("Signin")}>
+        <Spacer>
+          <Text style={styles.link}>
+            Already have an account? Sign in instead!
+          </Text>
+        </Spacer>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -56,11 +42,10 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     marginBottom: 250,
   },
-  errorMessage: {
-    fontSize: 16,
-    color: "red",
+
+  link: {
+    color: "blue",
     marginLeft: 15,
-    marginTop: 15,
   },
 });
 
