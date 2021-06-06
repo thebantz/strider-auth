8315c7e10ee3c6483f3ba5fb4069119fa7e48445
“thebantz” <nicoledbanta@gmail.com>
2021-06-06 13:49:21 -0700

    scaffold auth context

---
+++ src/context/AuthContext.js
@@ -0,0 +1,14 @@
+import createDataContext from "./createDataContext";
+
+const authReducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export const { Context, Provider } = createDataContext(
+  authReducer,
+  {},
+  { isSignedIn: false }
+);
