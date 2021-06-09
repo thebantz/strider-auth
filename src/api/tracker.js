0647a23aaa92ac43c916fe32dd98e4fb09ce8cd8
“thebantz” <nicoledbanta@gmail.com>
2021-06-09 03:13:55 -0700

    send tracks to backend

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,20 @@
 import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export default axios.create({
+const instance = axios.create({
   baseURL: "http://e95905ea16b4.ngrok.io",
 });
+
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+export default instance;
--- src/context/TrackContext.js
+++ src/context/TrackContext.js
@@ -1,4 +1,5 @@
 import createDataContext from "./createDataContext";
+import trackerApi from "../api/tracker";
 
 const trackReducer = (state, action) => {
   switch (action.type) {
@@ -7,9 +8,13 @@ const trackReducer = (state, action) => {
   }
 };
 
-const fetchTracks = (dispatch) => (something) => {};
-const createTrack = (dispatch) => (name, locations) => {
+const fetchTracks = (dispatch) => (something) => {
+  //   const res = await trackerApi.get("/tracks");
+  //   console.log(res);
+};
+const createTrack = (dispatch) => async (name, locations) => {
   console.log(name, locations.length);
+  await trackerApi.post("/tracks", { name, locations });
 };
 
 export const { Context, Provider } = createDataContext(
