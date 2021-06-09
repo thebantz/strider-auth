9e44c7176e685eb657341e674adc5eee6d89b50a
“thebantz” <nicoledbanta@gmail.com>
2021-06-08 20:40:10 -0700

    mock location data

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,14 +1,48 @@
-import React from "react";
+import "./_mockLocation";
+import React, { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 import { Text } from "react-native-elements";
 import { SafeAreaView } from "react-navigation";
+import {
+  requestForegroundPermissionsAsync,
+  watchPositionAsync,
+  Accuracy,
+} from "expo-location";
 import Map from "../components/Map";
 
 const TrackCreateScreen = () => {
+  const [err, setErr] = useState(null);
+
+  const startWatching = async () => {
+    try {
+      const { granted } = await requestForegroundPermissionsAsync();
+      if (!granted) {
+        throw new Error("Location permission not granted");
+      }
+      await watchPositionAsync(
+        {
+          accuracy: Accuracy.BestForNavigation,
+          timeInterval: 1000,
+          distanceInterval: 10,
+        },
+        (location) => {
+          console.log(location);
+        }
+      );
+    } catch (e) {
+      setErr(e);
+    }
+  };
+
+  useEffect(() => {
+    startWatching();
+  }, []);
+
   return (
     <SafeAreaView forceInset={{ top: "always" }}>
       <Text h3>Create A Track</Text>
       <Map />
+      {err ? <Text>Please enable location services</Text> : null}
     </SafeAreaView>
   );
 };
