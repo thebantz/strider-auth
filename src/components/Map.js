6e1e798edc38cd5af90824aee21de3ceb0cdace6
“thebantz” <nicoledbanta@gmail.com>
2021-06-08 21:49:08 -0700

    circle indicator to see where we are on the map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { ActivityIndicator } from "react-native";
 import { Text, StyleSheet } from "react-native";
-import MapView, { Polyline } from "react-native-maps";
+import MapView, { Polyline, Circle } from "react-native-maps";
 import { Context as LocationContext } from "../context/LocationContext";
 
 const Map = () => {
@@ -40,7 +40,14 @@ const Map = () => {
         latitudeDelta: 0.01,
         longitudeDelta: 0.01,
       }}
-    ></MapView>
+    >
+      <Circle
+        center={currentLocation.coords}
+        radius={30}
+        strokeColor="rgba(158, 158, 255, 1.0)"
+        fillColor="rgba(158, 158, 255, 0.3)"
+      />
+    </MapView>
   );
 };
 
