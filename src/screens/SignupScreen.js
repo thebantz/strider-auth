e5398e9a1428a13f973bc3a6aa7f471361cf0032
“thebantz” <nicoledbanta@gmail.com>
2021-06-08 17:56:13 -0700

    add clearErrorForm to SignupScreen

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,17 +1,17 @@
 import React, { useContext } from "react";
 import { View, StyleSheet, TouchableOpacity } from "react-native";
-import { Text } from "react-native-elements";
-import Spacer from "../components/Spacer";
+import { NavigationEvents } from "react-navigation";
 
 import { Context as AuthContext } from "../context/AuthContext";
 import AuthForm from "../components/AuthForm";
 import NavLink from "../components/NavLink";
 
 const SignupScreen = ({ navigation }) => {
-  const { state, signup } = useContext(AuthContext);
+  const { state, signup, clearErrorMessage } = useContext(AuthContext);
 
   return (
     <View style={styles.container}>
+      <NavigationEvents onWillFocus={clearErrorMessage} />
       <AuthForm
         headerText="Sign Up for Strider"
         errorMessage={state.errorMessage}
