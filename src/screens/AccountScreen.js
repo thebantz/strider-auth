0402d11591c0beeaf50e4f45dc8f08c94530007f
“thebantz” <nicoledbanta@gmail.com>
2021-06-08 18:25:17 -0700

    safeareaview for account screen

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,16 +1,17 @@
 import React, { useContext } from "react";
 import { View, StyleSheet, Text } from "react-native";
 import { Button } from "react-native-elements";
+import { SafeAreaView } from "react-navigation";
 import Spacer from "../components/Spacer";
 import { Context as AuthContext } from "../context/AuthContext";
 
 const AccountScreen = () => {
   const { signout } = useContext(AuthContext);
   return (
-    <>
+    <SafeAreaView forceInset={{ top: "always" }}>
       <Text style={{ fontSize: 48 }}>Account Screen</Text>
       <Button title="Sign Out" onPress={signout} />
-    </>
+    </SafeAreaView>
   );
 };
 const styles = StyleSheet.create({});
